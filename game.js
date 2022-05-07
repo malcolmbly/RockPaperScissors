07cd2392ebac2e11f69220c8bf02b8aae638ba01
malcolmbly <malcolmhbailey@gmail.com>
2022-05-07 17:29:47 -0600

    Added ComputerPlay() function

--- game.js
+++ game.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+console.log(computerPlay());
+
+function computerPlay() {
+    const r = Math.random();
+    if (r <= 1 / 3) {
+        return "Rock";
+    } else if (r <= 2 / 3) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
