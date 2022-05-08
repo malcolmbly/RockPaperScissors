64e8e496a89ee8f496d5ebfd657c6ea1b714309c
malcolmbly <malcolmhbailey@gmail.com>
2022-05-07 18:06:23 -0600

    Added game() method that controls game logic and starts game

--- game.js
+++ game.js
@@ -1,4 +1,27 @@
-console.log(computerPlay());
+let playerScore;
+let computerScore;
+
+game();
+
+function game() {
+    playerScore = 0;
+    computerScore = 0;
+    for (let i = 0; i < 5; i++){
+
+        let result = playRound(computerPlay(), playerPlay());
+
+        console.log(result);
+    }
+    console.log(`Final result is player: ${playerScore} and computer: ${computerScore}`);
+
+    if (playerScore > computerScore) {
+        console.log("Player wins!")
+    } else if (computerScore > playerScore) {
+        console.log("Computer wins!");
+    } else {
+        console.log("It's a tie!");
+    }
+}
 
 function computerPlay() {
     const r = Math.random();
@@ -8,5 +31,62 @@ function computerPlay() {
         return "Paper";
     } else {
         return "Scissors";
+    }
+}
+
+function playerPlay() {
+    let choice = prompt("Type rock, paper, or scissors.");
+    return choice;
+}
+
+function playRound(computerChoice, playerChoice) {
+    
+    if (playerChoice.toLowerCase() == "rock") {
+        if (computerChoice == "rock") {
+            //tie
+            return "It's a tie! Rock and Rock";
+            
+        } else if (computerChoice == "scissors") {
+            //player win
+            playerScore++;
+            return "You win! Rock beats scissors."
+
+        } else {
+            //computer win
+            computerScore++;
+            return "You lose! Paper beats rock."
+        }
+    } else if (playerChoice.toLowerCase() == "paper") {
+
+        if (computerChoice == "rock") {
+            //player win
+            playerScore++;
+            return "You win! Paper beats rock."
+
+        } else if (computerChoice == "scissors") {
+            //computer win
+            computerScore++;
+            return "You lose! Scissors beats paper."
+
+        } else {
+            //tie
+            return "It's a tie! Paper and Paper";
+        }
+    } else {
+        //player chose scissors
+        if (computerChoice == "rock") {
+            //computer win
+            computerScore++;
+            return "You lose! Rock beats scissors."
+            
+        } else if (computerChoice == "scissors") {
+            //tie
+            return "It's a tie! Scissors and Scissors";
+
+        } else {
+            //player win
+            playerScore++;
+            return "You win! Scissors beats paper."
+        }
     }
 }
